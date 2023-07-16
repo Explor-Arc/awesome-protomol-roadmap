95654b7b14d851b5439c77f91265330cb9ea0db3
Abubaker Siddique <Abubakersiddique761@gmail.com>
2023-07-16 10:40:55 +0530

    Create data.js

---
+++ JavaScript/data.js
@@ -0,0 +1,311 @@
+{
+    "Title": "JavaScript",
+    "Description": "This roadmap provides a comprehensive guide for learning JavaScript, starting from the basics and covering advanced topics. It is organized into five categories - JavaScript Fundamentals, DOM Mastery, Object Oriented JavaScript, Asynchronous JavaScript and APIs, and JavaScript Libraries and Frameworks - with more than 8 subtopics in each category. This roadmap is designed to help you learn JavaScript from a beginner to advanced level, and will provide you with a solid understanding of the language, as well as the tools and techniques used in modern JavaScript development. Whether you are a complete beginner, or looking to build upon your existing knowledge, this roadmap will give you a structured path to follow as you progress in your JavaScript journey.",
+    "Topics": [
+        {
+            "Title": "fundamentals",
+            "Description": "It covers the core concepts of the JavaScript programming language, including variables, data types, operators, control structures, functions, arrays, loops, strings, regular expressions, and math and date objects. These topics provide the foundation for further learning and development in JavaScript",
+            "Topics": [
+                {
+                    "Title": "Variables",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Data Types",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Operators",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Control Structures",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Functions",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Arrays",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Loops",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Strings",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Regular Expressions",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Math Object",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Date Object",
+                    "Link": "",
+                    "Priority": "2"
+                }
+            ]
+        },
+        {
+            "Title": "dom",
+            "Description": "Here, you will learn about the Document Object Model (DOM) and how to manipulate it using JavaScript. Topics include understanding the DOM, traversing and modifying elements, event handling, creating and removing elements, styling elements, forms and input validation, timers and intervals, and window and screen objects. With a mastery of the DOM, you will be able to create dynamic and interactive web pages.",
+            "Topics": [
+                {
+                    "Title": "Understanding the DOM",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Traversing and Modifying the DOM",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Event Handling",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Creating and Removing Elements",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Styling Elements with JavaScript",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Forms and Input Validation",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Timers and Intervals",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Window and Screen Objects",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Handling Browser Cookies",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Cross-browser Compatibility and Debugging",
+                    "Link": "",
+                    "Priority": "2"
+                }
+            ]
+        },
+        {
+            "Title": "object oriented",
+            "Description": "It focuses on object-oriented programming (OOP) concepts in JavaScript, including objects and properties, constructors and prototypes, classes and inheritance, function constructors, factory and constructor functions, ES6 classes, prototypal inheritance, and mixins. You will also learn about namespaces and modules, and design patterns in JavaScript. With a strong understanding of OOP in JavaScript, you will be able to write clean, maintainable, and scalable code.",
+            "Topics": [
+                {
+                    "Title": "Objects",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Constructors and Prototypes",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Classes",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Inheritance",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Function Constructors",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Factory Functions",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "ES6 Classes",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Prototypal Inheritance",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Mixins",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Namespaces and Modules",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Design Patterns in JavaScript",
+                    "Link": "",
+                    "Priority": "2"
+                }
+            ]
+        },
+        {
+            "Title": "asynchronous and apis",
+            "Description": "This category covers asynchronous programming in JavaScript and working with APIs. Topics include callbacks, promises, async/await, the Fetch API, AJAX and XHR, JSON and HTTP requests, REST APIs, web sockets, web workers and background tasks, and web storage and caching strategies. You will learn how to handle asynchronous events and perform multiple operations simultaneously, making your code more efficient and responsive.",
+            "Topics": [
+                {
+                    "Title": "Callbacks",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Promises",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Async/Await",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Fetch API",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "AJAX",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "XHR",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "JSON and HTTP Requests",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Working with REST APIs",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Web Sockets",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Web Workers and Background Tasks",
+                    "Link": "",
+                    "Priority": "2"
+                },
+                {
+                    "Title": "Web Storage",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Caching Strategies",
+                    "Link": "",
+                    "Priority": "1"
+                }
+            ]
+        },
+        {
+            "Title": "libraries and frameworks",
+            "Description": "Here, you will learn about popular JavaScript libraries and frameworks, including jQuery, React, Vue.js, and Angular. You will learn about single page application (SPA) development, routing and navigation, state management, and server-side rendering. You will also learn about animations and transitions, and testing and debugging in React and Vue.js. With a mastery of these libraries and frameworks, you will be able to build sophisticated and dynamic web applications.",
+            "Topics": [
+                {
+                    "Title": "jQuery",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "React",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Vue.js",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "Angular",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "SPA (Single Page Application) Development",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "Routing",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "Navigation",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "State Management",
+                    "Link": "",
+                    "Priority": "3"
+                },
+                {
+                    "Title": "Server-side Rendering",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Animations",
+                    "Link": "",
+                    "Priority": "1"
+                },
+                {
+                    "Title": "Transitions",
+                    "Link": "",
+                    "Priority": "1"
+                }
+            ]
+        }
+    ]
+}
